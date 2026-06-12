b49a25a3368d3974324810d9927fa336836f2f5a
Joshua <mmarshaljoshua34@gmail.com>
2026-06-12 21:13:14 +0530

    fixed cors

--- src/main/java/com/example/RMJHallAdmin/controller/BookingController.java
+++ src/main/java/com/example/RMJHallAdmin/controller/BookingController.java
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class BookingController {
     @Autowired
     BookingService bookingService;
